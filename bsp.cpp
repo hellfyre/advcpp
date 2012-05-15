5613e8f449cafca1c5f96d088f651ad89172afa2
Heinrich Schmidt <ktrask@ktrask.de>
2012-05-15 15:48:43 +0200

    pointer added a refcount

--- bsp.cpp
+++ bsp.cpp
@@ -3,12 +3,22 @@
 
 class MyClass {
 	public:
-	void doSomething(void) {std::cout << "hello world"<< std::endl;}
+	void doSomething(void) {std::cout << "test success"<< std::endl;}
 };
 
 
 int main()
 {
 	pointer<MyClass> p(new MyClass);
+
+	std::cout << "test first object:" << std::endl;
 	p->doSomething();
+	pointer<MyClass> v;
+
+	std::cout << "test copy:" << std::endl;
+	v=p;
+	v->doSomething();
+
+	//std::cout << "test change reference" << std::endl;
+	
 }
--- pointer.h
+++ pointer.h
@@ -1,9 +1,11 @@
 template <class T>
 class pointer{
-	T* ptr;
+	T* _ptr;
+	int* _refC;
 public:
-	explicit pointer(T* p = 0) : ptr(p) {}
-	~pointer() {delete ptr;}
-	T& operator*() { return *ptr;}
-	T* operator->() {return ptr;}
+	explicit pointer(T* p = 0) : _ptr(p) {_refC = new(int); (*_refC)=1;}
+	~pointer() {(*_refC)--;if((*_refC) == 0) {delete _ptr;delete _refC;}}
+	T& operator*() { return *_ptr;}
+	T* operator->() {return _ptr;}
+	//T
 };
