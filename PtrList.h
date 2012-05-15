c820b1c9f403275e5cf178a158f3bd912279d3b3
Heinrich Schmidt <ktrask@ktrask.de>
2012-05-15 16:53:14 +0200

    bug fixes in ptrlist

--- PtrList.h
+++ PtrList.h
@@ -1,20 +1,20 @@
 class PtrList {
   public:
-  PtrList *prev, next;
+  PtrList *prev, *next;
 
   PtrList() {
     prev = next = this;
   }
 
-  insert() {
-    PtrList elem = new PtrList();
-    elem.prev = prev;
-    elem.next = this;
-    prev->next = &elem;
-    prev = &elem;
+  void insert() {
+    PtrList* elem = new PtrList();
+    elem->prev = prev;
+    elem->next = this;
+    prev->next = elem;
+    prev = elem;
   }
 
-  erase() {
+  void erase() {
     this->next->prev = this->prev;
     this->prev->next = this->next;
   }
